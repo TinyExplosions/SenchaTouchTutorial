4c575e8b3d2623ee0c09b0b975625db59af72a98
Al Graham <alastair.graham@feedhenry.com>
2013-05-03 15:32:36 +0100

    adding some more cloud stuff

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,13 @@
+var data = [
+    {comment: 'Have you seen my weasel?', email: 'jane@blogs.com', name: 'Jane Blogs'},
+    {comment: 'I bent my wookee', email: 'ralph@wiggum.com', name: 'Ralph Wiggum'},
+    {comment: 'The new Celine Dion Album is rocking', email: 'noone@ever.com', name: 'Noone, Ever'}
+];
+
 exports.submitCommentForm = function(params, callback) {
   return callback(null, params);
+};
+
+exports.getCommentData = function(params, callback) {
+    return callback(null, data);
 };
